386e5e6fde580b932a5f228dedbc8568cfcc70ef
natwPine <natw.mezza@hotmail.fr>
2020-04-18 19:27:45 +0200

    Ajout couleur

---
+++ Keep it Alive/Assets/Scripts/MyColor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MyColor 
+{
+    [SerializeField] string name;
+    [SerializeField] Color color;
+
+    #region GETTER && SETTER
+    public Color Color { get => color; set => color = value; }
+    public string Name { get => name; set => name = value; }
+    #endregion
+}
--- Keep it Alive/Assets/Scripts/PixelBehaviours.cs
+++ Keep it Alive/Assets/Scripts/PixelBehaviours.cs
@@ -4,40 +4,64 @@ using UnityEngine;
 
 public class PixelBehaviours : MonoBehaviour
 {
-    [SerializeField] private int sun, moon, wind, rain = 0;
+    [SerializeField] private List<string> affect;
 
-    [SerializeField] Renderer myMat;
+    [SerializeField] private float sun, moon, wind, rain = 0;
+     Renderer myMat;
+    float reduce = 0;
+    [SerializeField] MyColor[] mycolor;
 
     // Start is called before the first frame update
     void Start()
     {
         myMat = GetComponent<Renderer>();
     }
+    private void Update()
+    {
+
+        reduce = Mathf.Lerp(0, sun, .5f);
+        Debug.Log(reduce);
+        /*if (affect.Count == 0 && sun > 0)
+        {
+            Debug.Log("ok");
+            float reduce = Mathf.Lerp(0, sun, 20);
+            Debug.Log(reduce);
+            sun -= reduce;
+        }*/
+    }
 
     void add(string objectTag)
     {
-        switch (objectTag)
+        if(affect.Count != 0)
         {
-            case "SUN":
-                sun++;
-                break;
+            switch (objectTag)
+            {
+                case "SUN":
+                    sun++;
+                    break;
 
-            case "MOON":
-                moon++;
-                break;
+                case "MOON":
+                    moon++;
+                    break;
 
-            case "WIND":
-                wind++;
-                break;
+                case "WIND":
+                    wind++;
+                    break;
 
-            case "RAIN":
-                rain++;
-                break;
+                case "RAIN":
+                    rain++;
+                    break;
 
-            default:
-                Debug.Log("stop");
-                break;
+                default:
+                    Debug.Log("stop");
+                    break;
+            }
         }
+        else
+        {
+            sun = (int)Mathf.Lerp(sun, 0, 10);
+        }
+        
         VerifColor();
     }
 
@@ -45,7 +69,40 @@ public class PixelBehaviours : MonoBehaviour
     {
         if(sun > 4)
         {
-            myMat.material.SetColor("_Color", new Color(122,201,12));
+            UpdateLand("foret");
+        }
+    }
+
+    private void UpdateLand(string newLand)
+    {
+        switch (newLand) {
+            case "montagne":
+                myMat.material.SetColor("_Color", mycolor[0].Color);
+                break;
+
+            case "tundra":
+                myMat.material.SetColor("_Color", mycolor[1].Color);
+                break;
+
+            case "lac":
+                myMat.material.SetColor("_Color", mycolor[2].Color);
+                break;
+
+            case "desert":
+                myMat.material.SetColor("_Color", mycolor[3].Color);
+                break;
+
+            case "prairie":
+                myMat.material.SetColor("_Color", mycolor[4].Color);
+                break;
+
+            case "foret":
+                myMat.material.SetColor("_Color", mycolor[5].Color);
+                break;
+
+            case "ocean":
+                myMat.material.SetColor("_Color", mycolor[6].Color);
+                break;
         }
     }
 
@@ -53,7 +110,16 @@ public class PixelBehaviours : MonoBehaviour
     {
         if(other.tag != null)
         {
+            affect.Add(other.gameObject.name);
             add(other.tag);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag != null)
+        {
+            affect.Remove(other.gameObject.name);
         }
     }
 }
